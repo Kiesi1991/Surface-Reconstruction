4f3245518ed0e1f424a503777a542568f678b5b5
markus <kiesenhofer.markus@outlook.com>
2022-04-17 19:04:15 +0200

    22

--- mainFilament.py
+++ mainFilament.py
@@ -12,261 +12,210 @@ import glob
 from torchvision import transforms
 import imageio
 import cv2
-
 from models import *
 
-# surface properties
-length = 2
-width = 2
 
-resolution = (386, 516)
+def train_NN(camera, lights, light_intensity, rough, diffuse, f0P):
+    resolution = (386, 516)
 
-# training parameters
-num_epochs = 40
-lr = 1e-4
-crop = 50
+    # training parameters
+    num_epochs = 40
+    lr = 1e-4
+    crop = 50
 
-file_path = os.path.join('realSamples', '**', f'*.jpg')
-paths = glob.glob(file_path, recursive=True)
-numbers = [x[-6:-4] for x in paths]
+    file_path = os.path.join('realSamples', '**', f'*.jpg')
+    paths = glob.glob(file_path, recursive=True)
+    numbers = [x[-6:-4] for x in paths]
 
-images = [None]*len(numbers)
-for idx, number in enumerate(numbers):
-    if number[0] == '/':
-        number = number[1]
-    images[int(number)] = paths[idx]
+    images = [None]*len(numbers)
+    for idx, number in enumerate(numbers):
+        if number[0] == '/':
+            number = number[1]
+        images[int(number)] = paths[idx]
 
-convert_tensor = transforms.ToTensor()
+    convert_tensor = transforms.ToTensor()
 
-samples = None
-for image in images:
-    try:
-        imageGrayscale = imageio.imread(image)
-    except:
-        pass
-    im = convert_tensor(Image.fromarray(imageGrayscale))[0].unsqueeze(0)
-    if samples == None:
-        samples = im
+    samples = None
+    for image in images:
+        try:
+            imageGrayscale = imageio.imread(image)
+        except:
+            pass
+        im = convert_tensor(Image.fromarray(imageGrayscale))[0].unsqueeze(0)
+        if samples == None:
+            samples = im
+        else:
+            samples = torch.cat((samples, im), dim=0)
+
+    model = ResidualNetwork()
+    if torch.cuda.is_available():
+        device = 'cuda'
+        model.to(device)
     else:
-        samples = torch.cat((samples, im), dim=0)
+        device = 'cpu'
 
-h1, h2, h3 = 0.79, 3.29, 5.79
-r1, r2, r3 = 2.975, 2.660705446, 1.937933168
+    shader = FilamentShading(camera, lights, light_intensity, rough=rough, diffuse=diffuse, f0P=f0P, device=device)
 
-locationLights = [[0.0, -r3, h3], [r3, 0.0, h3],
-                  [0.0, r3, h3],  [-r3, 0.0, h3],
-                  [0.0, -r2, h2], [r2, 0.0, h2],
-                  [0.0, r2, h2],  [-r2, 0.0, h2],
-                  [0.0, -r1, h1], [r1, 0.0, h1],
-                  [0.0, r1, h1],  [-r1, 0.0, h1]]
+    dataset = DummySet(resolution)
 
+    n_samples = len(dataset)
+    # Shuffle integers from 0 n_samples to get shuffled sample indices
+    shuffled_indices = np.random.permutation(n_samples)
+    testset_inds = shuffled_indices[:n_samples//10]
+    trainingset_inds = shuffled_indices[n_samples//10:]
 
-lights = torch.tensor(locationLights)
+    # Create PyTorch subsets from our subset-indices
+    testset = Subset(dataset, indices=testset_inds)
+    trainingset = Subset(dataset, indices=trainingset_inds)
 
-cameraDistance = 8.
-camera = torch.tensor([[[[[0, 0, cameraDistance]]]]])
+    testloader = DataLoader(testset, batch_size=1, shuffle=False)
+    trainloader = DataLoader(trainingset, batch_size=4, shuffle=True)
 
-show_fake = True
-if show_fake:
-    surface = torch.tensor(createSurface(resolution).tolist()).unsqueeze(0)
-    shader = FilamentShading(camera, lights, device='cpu')
-    im = shader.forward(surface)
+    path = os.path.join('results', '205', 'Epoch-2800')
+    optimized_surface = torch.load(os.path.join(path, 'surface.pt'))
 
-    for im_nr in range(12):
-        plt.imshow(im[0,im_nr,:,:].cpu().detach().numpy())
-        plt.show()
+    ############################################################################
+    # Update and evaluate network
+    ############################################################################
 
-model = ResidualNetwork()
-if torch.cuda.is_available():
-    device = 'cuda'
-    model.to(device)
-else:
-    device = 'cpu'
+    def _forward(network: nn.Module, data: DataLoader, metric: callable):
+        device = next(network.parameters()).device
 
-shader = FilamentShading(camera, lights, device=device)
+        for j, (surface, idx) in enumerate(data):
+            surface = surface.to(device)
+            x = shader.forward(surface)
 
-dataset = DummySet(resolution)
+            pred = model(x)
+            pred = shader.forward((pred))
 
-n_samples = len(dataset)
-# Shuffle integers from 0 n_samples to get shuffled sample indices
-shuffled_indices = np.random.permutation(n_samples)
-testset_inds = shuffled_indices[:n_samples//10]
-trainingset_inds = shuffled_indices[n_samples//10:]
+            res = metric(pred[:,:,crop:-crop,crop:-crop], x[:,:,crop:-crop,crop:-crop])
 
-# Create PyTorch subsets from our subset-indices
-testset = Subset(dataset, indices=testset_inds)
-trainingset = Subset(dataset, indices=trainingset_inds)
+            yield res
 
-testloader = DataLoader(testset, batch_size=1, shuffle=False)
-trainloader = DataLoader(trainingset, batch_size=4, shuffle=True)
+    @torch.no_grad()
+    def evaluate(network: nn.Module, data: DataLoader, metric: callable) -> list:
+        network.eval()
 
-path = os.path.join('results', '205', 'Epoch-2800')
-optimized_surface = torch.load(os.path.join(path, 'surface.pt'))
-
-############################################################################
-# Update and evaluate network
-############################################################################
-
-def _forward(network: nn.Module, data: DataLoader, metric: callable):
-    device = next(network.parameters()).device
-
-    for j, (surface, idx) in enumerate(data):
-        surface = surface.to(device)
-        x = shader.forward(surface)
-
-        pred = model(x)
-        pred = shader.forward((pred))
-
-        res = metric(pred[:,:,crop:-crop,crop:-crop], x[:,:,crop:-crop,crop:-crop])
-
-        yield res
-
-@torch.no_grad()
-def evaluate(network: nn.Module, data: DataLoader, metric: callable) -> list:
-    network.eval()
-
-    results = _forward(network, data, metric)
-    return [res.item() for res in results]
+        results = _forward(network, data, metric)
+        return [res.item() for res in results]
 
 
-@torch.enable_grad()
-def update(network: nn.Module, data: DataLoader, loss: nn.Module,
-           opt: torch.optim.Optimizer) -> list:
+    @torch.enable_grad()
+    def update(network: nn.Module, data: DataLoader, loss: nn.Module,
+               opt: torch.optim.Optimizer) -> list:
 
-    network.train()
+        network.train()
 
-    errs = []
-    for idx, err in enumerate(_forward(network, data, loss)):
-        errs.append(err.item())
+        errs = []
+        for idx, err in enumerate(_forward(network, data, loss)):
+            errs.append(err.item())
 
-        opt.zero_grad()
-        (err).backward()
-        opt.step()
-    return errs
+            opt.zero_grad()
+            (err).backward()
+            opt.step()
+        return errs
 
-############################################################################
-# training and evaluation
-############################################################################
+    ############################################################################
+    # training and evaluation
+    ############################################################################
 
-mse = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=0.0)
+    mse = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=0.0)
 
-path = os.path.join('results')
+    path = os.path.join('results')
 
-folder = 0
-while True:
-    if not os.path.exists(os.path.join(path)):
-        os.mkdir(os.path.join(path))
-    if not os.path.exists(os.path.join(path, f'{folder}')):#, f'sigma={sigma},p={p}')):
-        os.mkdir(os.path.join(path, f'{folder}'))#, f'sigma={sigma},p={p}'))
-        path = os.path.join(path, f'{folder}')#, f'sigma={sigma},p={p}')
-        break
-    else:
-        folder += 1
+    folder = 0
+    while True:
+        if not os.path.exists(os.path.join(path)):
+            os.mkdir(os.path.join(path))
+        if not os.path.exists(os.path.join(path, f'{folder}')):
+            os.mkdir(os.path.join(path, f'{folder}'))
+            path = os.path.join(path, f'{folder}')
+            break
+        else:
+            folder += 1
 
-im_nr = [1, 5]
-diff = []
-diff_surface = []
-vals = []
-trains = []
-for epoch in range(num_epochs):
-    os.mkdir(os.path.join(path, f'{epoch}'))
-    errs = update(model, trainloader, mse, optimizer)
-    val_errs = evaluate(model, testloader, mse)
+    diff = []
+    diff_surface = []
+    vals = []
+    trains = []
+    for epoch in range(num_epochs):
+        os.mkdir(os.path.join(path, f'{epoch}'))
+        errs = update(model, trainloader, mse, optimizer)
+        val_errs = evaluate(model, testloader, mse)
 
-    surface_im = testset[0][0].unsqueeze(0).cuda() # 1,H,W
-    im = shader.forward(surface_im) # 1,L,H,W
-    pred = model(im) # 1,H,W
+        surface_im = testset[0][0].unsqueeze(0).cuda() # 1,H,W
+        im = shader.forward(surface_im) # 1,L,H,W
+        pred = model(im) # 1,H,W
 
-    mse_surface = mse(surface_im, pred).item()
+        mse_surface = mse(surface_im, pred).item()
 
-    diff.append(mse_surface)
-    vals.append(mean(val_errs))
-    trains.append(mean(errs))
+        diff.append(mse_surface)
+        vals.append(mean(val_errs))
+        trains.append(mean(errs))
 
-    true_mse_surface = torch.log(mse(optimized_surface, pred.cpu().detach())+1).item()
-    diff_surface.append(true_mse_surface)
-    for L in range(12):
-        plt.figure(figsize=(20, 10))
-        plt.subplot(1, 2, 1)
-        plt.imshow(samples[L, crop:-crop, crop:-crop].cpu().detach().numpy())
-        plt.clim(0, 1.0)
+        true_mse_surface = torch.log(mse(optimized_surface, pred.cpu().detach())+1).item()
+        diff_surface.append(true_mse_surface)
+        for L in range(12):
+            plt.figure(figsize=(20, 10))
+            plt.subplot(1, 2, 1)
+            plt.imshow(samples[L, crop:-crop, crop:-crop].cpu().detach().numpy())
+            plt.clim(0, 1.0)
 
-        pred = model(samples.unsqueeze(0).to(next(model.parameters()).device))
-        pred = shader.forward(pred).squeeze(0)
-        plt.subplot(1, 2, 2)
-        plt.imshow(pred[L, crop:-crop, crop:-crop].cpu().detach().numpy())
-        plt.clim(0, 1.0)
+            pred = model(samples.unsqueeze(0).to(next(model.parameters()).device))
+            pred = shader.forward(pred).squeeze(0)
+            plt.subplot(1, 2, 2)
+            plt.imshow(pred[L, crop:-crop, crop:-crop].cpu().detach().numpy())
+            plt.clim(0, 1.0)
 
-        plt.savefig(os.path.join(path, f'{epoch}', f'True-{L}.png'))
-        plt.close()
+            plt.savefig(os.path.join(path, f'{epoch}', f'True-{L}.png'))
+            plt.close()
 
-        plt.imshow(im.squeeze(0)[L, crop:-crop, crop:-crop].cpu().detach().numpy())
-        plt.clim(0, 1.0)
-        plt.savefig(os.path.join(path, f'{epoch}', f'Fake-{L}.png'))
-        plt.close()
+            plt.imshow(im.squeeze(0)[L, crop:-crop, crop:-crop].cpu().detach().numpy())
+            plt.clim(0, 1.0)
+            plt.savefig(os.path.join(path, f'{epoch}', f'Fake-{L}.png'))
+            plt.close()
 
-        p = cv2.cvtColor(pred[L, crop:-crop, crop:-crop].cpu().detach().numpy(), cv2.COLOR_GRAY2RGB)
-        t = cv2.cvtColor(samples[L, crop:-crop, crop:-crop].cpu().detach().numpy(), cv2.COLOR_GRAY2RGB)
+            p = cv2.cvtColor(pred[L, crop:-crop, crop:-crop].cpu().detach().numpy(), cv2.COLOR_GRAY2RGB)
+            t = cv2.cvtColor(samples[L, crop:-crop, crop:-crop].cpu().detach().numpy(), cv2.COLOR_GRAY2RGB)
 
-        plt.figure(figsize=(20, 10))
-        plt.subplot(1, 2, 1)
-        plt.imshow(t)
-        plt.clim(0, 1.0)
+            plt.figure(figsize=(20, 10))
+            plt.subplot(1, 2, 1)
+            plt.imshow(t)
+            plt.clim(0, 1.0)
 
-        plt.subplot(1, 2, 2)
-        plt.imshow(p)
-        plt.clim(0, 1.0)
+            plt.subplot(1, 2, 2)
+            plt.imshow(p)
+            plt.clim(0, 1.0)
 
-        plt.savefig(os.path.join(path, f'{epoch}', f'TrueRGB-{L}.png'))
-        plt.close()
+            plt.savefig(os.path.join(path, f'{epoch}', f'TrueRGB-{L}.png'))
+            plt.close()
 
-        x = np.linspace(0, len(diff_surface) - 1, len(diff_surface))
-        plt.plot(x, diff_surface, label='difference')
-        plt.xlabel('epoch')
-        plt.ylabel('Error')
-        plt.legend()
-        plt.savefig(os.path.join(path, f'difference-surface.png'))
-        plt.close()
+            x = np.linspace(0, len(diff_surface) - 1, len(diff_surface))
+            plt.plot(x, diff_surface, label='difference')
+            plt.xlabel('epoch')
+            plt.ylabel('Error')
+            plt.legend()
+            plt.savefig(os.path.join(path, f'difference-surface.png'))
+            plt.close()
 
-        x = np.linspace(0, len(diff) - 1, len(diff))
-        plt.plot(x, diff, label='difference')
-        plt.plot(x, vals, label='validation')
-        plt.plot(x, trains, label='training')
-        plt.xlabel('epoch')
-        plt.ylabel('Error')
-        plt.legend()
-        plt.savefig(os.path.join(path, f'training-errors.png'))
-        plt.close()
+            x = np.linspace(0, len(diff) - 1, len(diff))
+            plt.plot(x, diff, label='difference')
+            plt.plot(x, vals, label='validation')
+            plt.plot(x, trains, label='training')
+            plt.xlabel('epoch')
+            plt.ylabel('Error')
+            plt.legend()
+            plt.savefig(os.path.join(path, f'training-errors.png'))
+            plt.close()
 
-        torch.save(model.state_dict(), os.path.join(path, f'{epoch}', 'model.pt'))
+            torch.save(model.state_dict(), os.path.join(path, f'{epoch}', 'model.pt'))
 
-        # loading model
-        '''
-        model = TheModelClass(*args, **kwargs)
-        model.load_state_dict(torch.load(PATH))
-        model.eval()
-        '''
+            # loading model
+            '''
+            model = TheModelClass(*args, **kwargs)
+            model.load_state_dict(torch.load(PATH))
+            model.eval()
+            '''
 
-
-
-
-    '''for light in im_nr:
-        im_target = im[0][light].unsqueeze(2).repeat(1, 1, 3)
-        im_pred = shader.forward(pred)[0][light].unsqueeze(2).repeat(1, 1, 3)
-
-        plt.figure(figsize=(20, 10))
-        plt.subplot(1, 2, 1)
-        plt.imshow(im_target.cpu().detach().numpy())
-
-        plt.subplot(1, 2, 2)
-        plt.imshow(im_pred.cpu().detach().numpy())
-
-        plt.savefig(os.path.join(path, f'{epoch}-{light}.png'))
-        plt.close()'''
-
-    print(f'Epoch {epoch} AVG Mean {mean(errs):.6f} AVG Val Mean {mean(val_errs):.6f} MSE Surface {mse_surface}')
-
-
-
-print('TheEnd')
+        print(f'Epoch {epoch} AVG Mean {mean(errs):.6f} AVG Val Mean {mean(val_errs):.6f} MSE Surface {mse_surface}')
--- optimize_parameters.py
+++ optimize_parameters.py
@@ -6,171 +6,153 @@ from torchvision import transforms
 from models import OptimizeParameters
 import matplotlib.pyplot as plt
 import cv2
-
-
-resolution = (386, 516)
-
-cameraDistance = 8.
-camera = torch.tensor([[[[[0, 0, cameraDistance]]]]])
-
-h1, h2, h3 = 0.79, 3.29, 5.79
-r1, r2, r3 = 2.975, 2.660705446, 1.937933168
-
-
-locationLights = [[0.0, -r3, h3], [r3, 0.0, h3],
-                  [0.0, r3, h3],  [-r3, 0.0, h3],
-                  [0.0, -r2, h2], [r2, 0.0, h2],
-                  [0.0, r2, h2],  [-r2, 0.0, h2],
-                  [0.0, -r1, h1], [r1, 0.0, h1],
-                  [0.0, r1, h1],  [-r1, 0.0, h1]]
-
-'''locationLights = [[ 0.5025, -2.4060,  1.3907],
-        [ 2.3715, -0.2657,  1.3804],
-        [-0.1163,  2.3329,  1.4274],
-        [-2.2779,  0.3018,  1.4543],
-        [ 0.3971, -2.4663,  3.6350],
-        [ 2.4868, -0.2689,  3.4896],
-        [-0.0831,  2.6326,  3.3804],
-        [-2.6358,  0.6189,  3.2640],
-        [ 0.2359, -2.4625,  5.0852],
-        [ 2.5166, -0.1509,  5.1509],
-        [ 0.2503,  2.2478,  5.2799],
-        [-2.3585, -0.1042,  5.3759]]'''
-
-lights = torch.tensor(locationLights)
-
-file_path = os.path.join('realSamples', '**', f'*.jpg')
-paths = glob.glob(file_path, recursive=True)
-numbers = [x[-6:-4] for x in paths]
-
-images = [None]*len(numbers)
-for idx, number in enumerate(numbers):
-    if number[0] == '/':
-        number = number[1]
-    images[int(number)] = paths[idx]
-
-convert_tensor = transforms.ToTensor()
-
-samples = None
-for image in images:
-    try:
-        imageGrayscale = imageio.imread(image)
-    except:
-        pass
-    im = convert_tensor(Image.fromarray(imageGrayscale))[0].unsqueeze(-1)
-    if samples == None:
-        samples = im
-    else:
-        samples = torch.cat((samples, im), dim=-1)
-
-mesh = torch.zeros(resolution).unsqueeze(0)
-
-model = OptimizeParameters(mesh, lights, camera)
-
-color = model.forward()
-
-from PIL import Image
 import numpy as np
 import statistics
-'''
-for idx in range(8):
-    im = Image.fromarray(np.uint8(color[:,:,idx].detach().numpy()*255))
-    im.show()
-    #ims = Image.fromarray(np.uint8(samples[:, :, idx].detach().numpy() * 255))
-    #ims.show()
-    print(idx)'''
 
-path = os.path.join('results')
+def optimizeParameters(path_target='realSamples', path_results='results', lr = 1e-3, epochs = 3000):
+    resolution = (386, 516)
 
-folder = 0
-while True:
-    if not os.path.exists(os.path.join(path)):
-        os.mkdir(os.path.join(path))
-    if not os.path.exists(os.path.join(path, f'{folder}')):#, f'sigma={sigma},p={p}')):
-        os.mkdir(os.path.join(path, f'{folder}'))#, f'sigma={sigma},p={p}'))
-        path = os.path.join(path, f'{folder}')#, f'sigma={sigma},p={p}')
-        break
-    else:
-        folder += 1
+    cameraDistance = 8.
+    camera = torch.tensor([[[[[0, 0, cameraDistance]]]]])
 
-lr = 1e-3
-mse = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=0.0)
+    h1, h2, h3 = 0.79, 3.29, 5.79
+    r1, r2, r3 = 2.975, 2.660705446, 1.937933168
 
-epochs = 10001
-errs = []
-errors = []
-for epoch in range(epochs):
-        pred = model.forward()
-        err = mse(pred, samples)
-        errs.append(err.item())
-        optimizer.zero_grad()
-        err.backward()
-        torch.nn.utils.clip_grad_value_(model.mesh, 0.001)
-        optimizer.step()
-        if epoch % 10 == 0:
-            print(f'Epoch {epoch} AVG Err {statistics.mean(errs[-10:])} Surface Max {model.mesh.detach().max()} Surface Min {model.mesh.detach().min()}')
-            errors.append(statistics.mean(errs[-10:]))
-        if epoch % 100 == 0:
-            os.mkdir(os.path.join(path, f'Epoch-{epoch}'))
-            path2 = os.path.join(path, f'Epoch-{epoch}')
-            print(f'Rough {model.rough.item()} Diffuse {model.diffuse.item()} f0P {model.f0P.item()} Camera {model.camera.detach()}')
-            num_L = samples.shape[2]
-            for L in range(num_L):
-                plt.figure(figsize=(20, 10))
-                plt.subplot(1, 2, 1)
 
-                plt.imshow(samples[:,:,L].cpu().detach().numpy())
-                plt.clim(0, 1.0)
+    locationLights = [[0.0, -r3, h3], [r3, 0.0, h3],
+                      [0.0, r3, h3],  [-r3, 0.0, h3],
+                      [0.0, -r2, h2], [r2, 0.0, h2],
+                      [0.0, r2, h2],  [-r2, 0.0, h2],
+                      [0.0, -r1, h1], [r1, 0.0, h1],
+                      [0.0, r1, h1],  [-r1, 0.0, h1]]
 
-                plt.subplot(1, 2, 2)
+    lights = torch.tensor(locationLights)
 
-                plt.imshow(pred[:,:,L].cpu().detach().numpy())
-                plt.clim(0, 1.0)
+    file_path = os.path.join(path_target, '**', f'*.jpg')
+    paths = glob.glob(file_path, recursive=True)
+    numbers = [x[-6:-4] for x in paths]
 
-                plt.savefig(os.path.join(path2, f'{L}.png'))
-                plt.close()
+    images = [None]*len(numbers)
+    for idx, number in enumerate(numbers):
+        if number[0] == '/':
+            number = number[1]
+        images[int(number)] = paths[idx]
 
-                p = cv2.cvtColor(pred[:,:,L].cpu().detach().numpy(), cv2.COLOR_GRAY2RGB)
-                t = cv2.cvtColor(samples[:,:,L].cpu().detach().numpy(), cv2.COLOR_GRAY2RGB)
+    convert_tensor = transforms.ToTensor()
 
-                plt.figure(figsize=(20, 10))
-                plt.subplot(1, 2, 1)
-                plt.imshow(t)
-                plt.clim(0, 1.0)
+    samples = None
+    for image in images:
+        try:
+            imageGrayscale = imageio.imread(image)
+        except:
+            pass
+        im = convert_tensor(Image.fromarray(imageGrayscale))[0].unsqueeze(-1)
+        if samples == None:
+            samples = im
+        else:
+            samples = torch.cat((samples, im), dim=-1)
 
-                plt.subplot(1, 2, 2)
-                plt.imshow(p)
-                plt.clim(0, 1.0)
+    mesh = torch.zeros(resolution).unsqueeze(0)
 
-                plt.savefig(os.path.join(path2, f'TrueRGB-{L}.png'))
-                plt.close()
+    model = OptimizeParameters(mesh, lights, camera)
 
-                x = np.linspace(0, len(errors) - 1, len(errors))
-                plt.plot(x, errors, label='errors')
-                plt.xlabel('epoch')
-                plt.ylabel('Error')
-                plt.legend()
-                plt.savefig(os.path.join(path, f'error.png'))
-                plt.close()
+    path = os.path.join(path_results)
 
-            with open(os.path.join(path2, 'parameters.txt'), 'w') as f:
-                f.write(f'Rough {model.rough.item()} Diffuse {model.diffuse.item()} f0P {model.f0P.item()} \n'
-                        f'Camera {model.camera.detach()}\n'
-                        f'Lights {model.lights.detach()}\n'
-                        f'Surface {model.mesh.detach()}\n'
-                        f'Surface Max {model.mesh.detach().max()}'
-                        f'Surface min {model.mesh.detach().min()}\n'
-                        f'Light Intensity {model.light_intensity.detach()}\n'
-                        f'Light Color {model.light_color.detach()}\n'
-                        f'X {model.x.detach()}\n'
-                        f'Y {model.y.detach()}\n'
-                        f'AVG Err {statistics.mean(errs[-10:])}')
+    folder = 0
+    while True:
+        if not os.path.exists(os.path.join(path)):
+            os.mkdir(os.path.join(path))
+        if not os.path.exists(os.path.join(path, f'{folder}')):
+            os.mkdir(os.path.join(path, f'{folder}'))
+            path = os.path.join(path, f'{folder}')
+            break
+        else:
+            folder += 1
 
-            torch.save(model.mesh.detach(), os.path.join(path2, 'surface.pt'))
 
-'''for idx in range(8):
-    im = Image.fromarray(np.uint8(pred[:,:,idx].detach().numpy()*255))
-    im.show()'''
+    mse = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=0.0)
 
-print('TheEnd')
+    errs = []
+    errors = []
+    for epoch in range(epochs):
+            pred = model.forward()
+            err = mse(pred, samples)
+            errs.append(err.item())
+            optimizer.zero_grad()
+            err.backward()
+            torch.nn.utils.clip_grad_value_(model.mesh, 0.001)
+            optimizer.step()
+            if epoch % 10 == 0:
+                # print(f'Epoch {epoch} AVG Err {statistics.mean(errs[-10:])} Surface Max {model.mesh.detach().max()} Surface Min {model.mesh.detach().min()}')
+                errors.append(statistics.mean(errs[-10:]))
+            if epoch % 100 == 0:
+                os.mkdir(os.path.join(path, f'Epoch-{epoch}'))
+                path2 = os.path.join(path, f'Epoch-{epoch}')
+                print(f'Rough {model.rough.item()} Diffuse {model.diffuse.item()} f0P {model.f0P.item()} Camera {model.camera.detach()}')
+                num_L = samples.shape[2]
+                for L in range(num_L):
+                    plt.figure(figsize=(20, 10))
+                    plt.subplot(1, 2, 1)
+
+                    plt.imshow(samples[:,:,L].cpu().detach().numpy())
+                    plt.clim(0, 1.0)
+
+                    plt.subplot(1, 2, 2)
+
+                    plt.imshow(pred[:,:,L].cpu().detach().numpy())
+                    plt.clim(0, 1.0)
+
+                    plt.savefig(os.path.join(path2, f'{L}.png'))
+                    plt.close()
+
+                    p = cv2.cvtColor(pred[:,:,L].cpu().detach().numpy(), cv2.COLOR_GRAY2RGB)
+                    t = cv2.cvtColor(samples[:,:,L].cpu().detach().numpy(), cv2.COLOR_GRAY2RGB)
+
+                    plt.figure(figsize=(20, 10))
+                    plt.subplot(1, 2, 1)
+                    plt.imshow(t)
+                    plt.clim(0, 1.0)
+
+                    plt.subplot(1, 2, 2)
+                    plt.imshow(p)
+                    plt.clim(0, 1.0)
+
+                    plt.savefig(os.path.join(path2, f'TrueRGB-{L}.png'))
+                    plt.close()
+
+                    x = np.linspace(0, len(errors) - 1, len(errors))
+                    plt.plot(x, errors, label='errors')
+                    plt.xlabel('epoch')
+                    plt.ylabel('Error')
+                    plt.legend()
+                    plt.savefig(os.path.join(path, f'error.png'))
+                    plt.close()
+
+                with open(os.path.join(path2, 'parameters.txt'), 'w') as f:
+                    f.write(f'Rough {model.rough.item()} Diffuse {model.diffuse.item()} f0P {model.f0P.item()} \n'
+                            f'Camera {model.camera.detach()}\n'
+                            f'Lights {model.lights.detach()}\n'
+                            f'Surface {model.mesh.detach()}\n'
+                            f'Surface Max {model.mesh.detach().max()}'
+                            f'Surface min {model.mesh.detach().min()}\n'
+                            f'Light Intensity {model.light_intensity.detach()}\n'
+                            f'Light Color {model.light_color.detach()}\n'
+                            f'X {model.x.detach()}\n'
+                            f'Y {model.y.detach()}\n'
+                            f'AVG Err {statistics.mean(errs[-10:])}')
+
+                torch.save(model.rough.detach().cpu(), os.path.join(path2, 'rough.pt'))
+                torch.save(model.diffuse.detach().cpu(), os.path.join(path2, 'diffuse.pt'))
+                torch.save(model.f0P.detach().cpu(), os.path.join(path2, 'f0P.pt'))
+                torch.save(model.camera.detach().cpu(), os.path.join(path2, 'camera.pt'))
+                torch.save(model.lights.detach().cpu(), os.path.join(path2, 'lights.pt'))
+                torch.save(model.light_intensity.detach(), os.path.join(path2, 'light_intensity.pt'))
+                torch.save(model.mesh.detach(), os.path.join(path2, 'surface.pt'))
+
+    return {'rough' : model.rough.detach().cpu(),
+            'diffuse' : model.diffuse.detach().cpu(),
+            'f0P' : model.f0P.detach().cpu(),
+            'camera' : model.camera.detach().cpu(),
+            'lights' : model.lights.detach().cpu(),
+            'light_intensity': model.light_intensity.detach().cpu()}
